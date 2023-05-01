16fde5649091ee5d546e94510d918444d3b86507
Vitor Cordeiro <vitor.cordeiro_in8@123milhas.com.br>
2023-05-01 20:54:02 -0300

    feat: criar usuario pelo dto

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,24 +3,25 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    const createUserInput: Prisma.UserCreateInput = {
-      name: '',
-      email: '',
-      cpf: '',
-      phone: '',
-      birthdate: '',
-      imagePath: '',
-    }
-    
+  async create(createUserDto: CreateUserDto) {
+    const hash = await bcrypt.hash(createUserDto.password, 10);
+
     return this.prisma.user.create({
-      data: createUserInput
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        cpf: createUserDto.cpf,
+        phone: createUserDto.phone,
+        birthdate: new Date(createUserDto.birthdate),
+        imagePath: createUserDto.imagePath,
+        password: hash,
+      },
     });
   }
 
@@ -28,38 +29,46 @@ export class UserService {
     return this.prisma.user.findMany();
   }
 
-  findOne(id: number) { 
+  findOne(id: number) {
     return this.prisma.user.findUnique({
       where: {
-        id: id
-      }
-    })
+        id: id,
+      },
+    });
+  }
+
+  findByEmail(email: string) {
+    console.log(email);
+    return this.prisma.user.findUnique({
+      where: {
+        email
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     const updateUserInput: Prisma.UserUpdateInput = {
-      id: 0,
       name: '',
       email: '',
       cpf: '',
       phone: '',
       birthdate: '',
       imagePath: '',
-    }
-    
+    };
+
     return this.prisma.user.update({
       data: updateUserInput,
       where: {
-        id: 0
-      }
+        id: 0,
+      },
     });
   }
 
   remove(id: number) {
     return this.prisma.user.delete({
       where: {
-        id: id
-      }
+        id: id,
+      },
     });
   }
 }
