3cc3a0687ed85909e80c56c8307f91fe2f9bac75
Lucas Ribino <lribino@outlook.com>
2023-05-07 16:19:39 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv'
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,  { cors: true });
   
   app.setGlobalPrefix('api')
   
